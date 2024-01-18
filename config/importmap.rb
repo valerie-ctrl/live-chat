869105adc59634ee95ea5086b51c358416765f31
Valeriia Chavdar <valera.chavdar@mail.ru>
2024-01-18 20:38:35 +0300

    Add stimlus controller, create a message partial

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,5 +1,5 @@
 # Pin npm packages by running ./bin/importmap
-
+pin "@rails/actioncable", to: "actioncable.esm.js"
 pin "application"
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js"
@@ -7,4 +7,3 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "bootstrap", to: "bootstrap.min.js", preload: true
 pin "@popperjs/core", to: "popper.js", preload: true
-pin "@rails/actioncable", from: jsdelivr
