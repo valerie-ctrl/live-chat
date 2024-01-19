72ff3232d8efc07dfd4c6d41d606030fb392bde8
Valeriia Chavdar <valera.chavdar@mail.ru>
2024-01-19 11:12:29 +0300

    Add a list of chatrooms and create action & form for chatroom

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -1,6 +1,23 @@
 class ChatroomsController < ApplicationController
   def show
+    @chatrooms = Chatroom.all
     @chatroom = Chatroom.find(params[:id])
+    @new_chatroom = Chatroom.new
     @message = Message.new
+  end
+
+  def create
+    @new_chatroom = Chatroom.new(new_chatroom_params)
+    if @new_chatroom.save
+      redirect_to chatrooms_path
+    else
+      render "chatrooms/show", status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def new_chatroom_params
+    params.require(:chatroom).permit(:name)
   end
 end
