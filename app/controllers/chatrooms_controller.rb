791d470f2f48bfe0d0b55f15bf1a0d15cbadaa0f
Valeriia Chavdar <valera.chavdar@mail.ru>
2024-01-19 12:42:06 +0300

    Add create private chat method to chatroomscontroller

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -1,5 +1,6 @@
 class ChatroomsController < ApplicationController
   def show
+    @users = User.where.not(id: current_user.id)
     @chatrooms = Chatroom.all
     @chatroom = Chatroom.find(params[:id])
     @new_chatroom = Chatroom.new
@@ -15,7 +16,15 @@ class ChatroomsController < ApplicationController
     end
   end
 
+  def create_private_chatroom
+    @new_private_chatroom = Chatroom.new
+  end
+
   private
+
+  def private_chatroom_params
+    params.require(:private_chatroom).permit(:user_id)
+  end
 
   def new_chatroom_params
     params.require(:chatroom).permit(:name)
