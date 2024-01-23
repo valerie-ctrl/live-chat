d295d3415d27f68ef529cbef69abe6f0b7eda491
Valeriia Chavdar <valera.chavdar@mail.ru>
2024-01-23 18:06:05 +0300

    Change home route to point to chatrooms show, also when thers no id

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,5 @@ class User < ApplicationRecord
   has_many :messages, dependent: :destroy
   has_many :chatroom_users, dependent: :destroy
   has_many :chatrooms, through: :chatroom_users
-  has_many :chatrooms
   validates_uniqueness_of :email, :nickname
 end
